933505ef3bc74e7049fac0a8e3ea7ede2b05da29
joaogkvalho <joaogkvalho@gmail.com>
2021-10-21 10:53:03 -0300

    css finished

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,52 @@
+import styles from './styles.module.scss'
+
+import logoImg from '../../assets/logo.svg'
+
 export function MessageList(){
     return(
-        <h1>Message List</h1>
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="DoWhile 2021" />
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Não vejo a hora de começar esse evento, com certeza vai
+                        ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/joaogkvalho.png" alt="João Gabriel" />
+                        </div>
+                        <span>João Gabriel</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Não vejo a hora de começar esse evento, com certeza vai
+                        ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/joaogkvalho.png" alt="João Gabriel" />
+                        </div>
+                        <span>João Gabriel</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Não vejo a hora de começar esse evento, com certeza vai
+                        ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/joaogkvalho.png" alt="João Gabriel" />
+                        </div>
+                        <span>João Gabriel</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
     )
 }
